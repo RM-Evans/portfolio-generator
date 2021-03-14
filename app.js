67afbdb466fd362c345ebbc5d1c1aef44a2f499e
RM-Evans <evansrichardmichael@gmail.com>
2021-03-13 19:45:54 -0700

    show stuff and then generate html file

---
+++ app.js
@@ -0,0 +1,40 @@
+//console.log('Hello Node!');  :)
+
+// let message = 'Hello Node!'
+
+// let sum = 5 + 3
+
+// console.log(message)
+// console.log(sum)
+
+//console.log(profileDataArgs)
+
+//console.log('================')
+
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i++){
+
+//         console.log(profileDataArr[i])
+//     }
+//     console.log('================')
+
+//     //is the same as
+
+//     // profileDataArr.forEach((profileItem) => {
+//     //     console.log(profileItem)
+//     // })
+
+//     //as well as
+
+//     profileDataArr.forEach(profileItem => console.log(profileItem))
+// }
+
+
+// printProfileData(profileDataArgs)
+
+// const name = profileDataArgs[0]
+// const github = profileDataArgs[1]
+
+
+
+const generatePage = require('./src/page-template.js');
