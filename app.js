5aaef825ad28d309647da9aaefc4ed5203e1f32e
RM-Evans <evansrichardmichael@gmail.com>
2021-03-15 18:28:04 -0600

    what is your name and gitignore

--- app.js
+++ app.js
@@ -36,5 +36,34 @@
 // const github = profileDataArgs[1]
 
 
+//==============
 
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
+
+
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
+
+// //const profileDataArgs = process.argv.slice(2);
+
+
+// //const [name, github] = profileDataArgs;
+
+
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
